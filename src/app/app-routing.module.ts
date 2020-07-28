ade167f47e39ec27f9fb8493c5f031b10b452f01
Stefano Notaro <stefano.notaro@visitor.upm.com>
2020-07-28 11:41:33 -0300

    ToDo component + Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToDoComponent } from './components/to-do/to-do.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ToDo', component: ToDoComponent },
+  { path: '*', pathMatch: 'full', redirectTo: 'ToDo' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
