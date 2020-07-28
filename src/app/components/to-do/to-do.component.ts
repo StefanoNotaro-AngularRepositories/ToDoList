169e8875b289d5bd95eaa494aa539cadecb38c29
Stefano Notaro <stefano.notaro@visitor.upm.com>
2020-07-28 16:27:39 -0300

    commit

--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
+import { Item } from '../../common/item.model';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
-  styleUrls: ['./to-do.component.css']
+  styleUrls: ['./to-do.component.css'],
 })
 export class ToDoComponent implements OnInit {
+  public toDoItems = [];
 
-  constructor() { }
+  public toDoGroup = new FormGroup({
+    toDoItem: new FormControl(''),
+    items: new FormArray(this.toDoItems),
+  });
 
-  ngOnInit(): void {
+  constructor() {
+    this.toDoItems.push(new FormControl({ checked: true, text: 'Test 1' }));
+    this.toDoItems.push(new FormControl({ checked: false, text: 'Test 2' }));
   }
 
+  ngOnInit(): void {}
+
+  public addNewItem() {
+    if (this.toDoGroup.controls.toDoItem.value) {
+      this.toDoItems.push(new FormControl({ checked: false, text: this.toDoGroup.controls.toDoItem.value }));
+      this.toDoGroup.controls.toDoItem.reset();
+    }
+  }
+
+  public itemDone(item: Item) {
+    item.checked = !item.checked;
+  }
+
+  public removeItem(i) {
+    this.toDoItems.splice(i, 1);
+  }
+
+  public reorderElements(elements) {
+    return _.sortBy(elements, (x) => {
+      return x.value.checked;
+    });
+  }
 }
